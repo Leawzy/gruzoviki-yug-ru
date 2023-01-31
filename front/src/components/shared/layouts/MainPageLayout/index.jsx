ccfb905b292a2a55d8e56c9de72cdf53e1f4e616
Renthop <onedostupov@gmail.com>
2023-01-31 23:33:31 +0300

    feat(front): prepare page to new components

---
+++ front/src/components/shared/layouts/MainPageLayout/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BaseLayout from "../BaseLayout/index.jsx";
+import CardLayout from "./CardLayout/index.jsx";
+
+function MainPageLayout() {
+    return (
+        <BaseLayout>
+            <CardLayout />
+        </BaseLayout>
+    );
+}
+
+export default MainPageLayout;
