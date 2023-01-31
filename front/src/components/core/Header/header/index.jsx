665c91a5ea6a3a52659b204071401b2628d9a0ba
Renthop <onedostupov@gmail.com>
2023-01-31 23:33:01 +0300

    feat(front): add Link to icons

--- front/src/components/core/Header/header/index.jsx
+++ front/src/components/core/Header/header/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 import account from '../../../../assets/image/icons/account.svg'
 import basket from '../../../../assets/image/icons/basket.svg'
 import favorite from '../../../../assets/image/icons/favorite.svg'
+
 import Navbar from "../navbar";
 
 function Header() {
@@ -22,19 +24,19 @@ function Header() {
                     <div className="header__contact">
                         <div className="header__contact-phone">
                             <a href="#">+8 989 777 42 45</a>
-                            <a className="header__contact-timetable">График: 8:00 до 19:00 СБ и ВСК выходной</a>
+                            <a className="header__contact-timetable">8:00 до 19:00 СБ и ВСК выходной</a>
                         </div>
                     </div>
                     <div>
-                        <a href="#" className="header__link-item">
+                        <Link to={'/profile'} className="header__link-item">
                             <img className="header__link-item-icon" width={34} height={34} src={account} alt="account"/>
-                        </a>
-                        <a href="#" className="header__link-item">
+                        </Link>
+                        <Link to={''} className="header__link-item">
                             <img className="header__link-item-icon" width={34} height={34} src={favorite} alt="favorite"/>
-                        </a>
-                        <a href="#" className="header__link-item">
+                        </Link>
+                        <Link to={''} className="header__link-item">
                             <img className="header__link-item-icon" width={34} height={34} src={basket} alt="basket"/>
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
