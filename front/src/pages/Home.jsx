b215392f7a61606d9ced4a4e6d605a937bb1697e
Renthop <onedostupov@gmail.com>
2023-01-31 23:12:45 +0300

    feat(front): creating Home page for APP

---
+++ front/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardLayout from "../components/shared/layouts/MainLayout/index.jsx";
+
+function Home() {
+    return (
+        <>
+            <CardLayout />
+        </>
+    );
+}
+
+export default Home;
