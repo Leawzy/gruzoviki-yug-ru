863d0adb6488f9ab4c5bd4744b691bdf53b86622
Renthop <onedostupov@gmail.com>
2023-02-11 10:36:56 +0300

    front(feat): creating function component for Card

--- front/src/components/features/home/components/NewCards/Card/index.jsx
+++ front/src/components/features/home/components/NewCards/Card/index.jsx
@@ -1,33 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import pictureOfItem from '../../../../../../assets/image/tovar-1.svg'
+import heartFavNeActive from '../../../../../../assets/image/icons/heartFav-neact.svg'
+import heartFavActive from '../../../../../../assets/image/icons/heartFav-act.svg'
+import plusBtnNeActive from '../../../../../../assets/image/icons/plusBtn-neact.svg'
+import plusBtnActive from '../../../../../../assets/image/icons/plusBtn-act.svg'
 
 import './card.scss'
-import pictureOfItem from '../../../../../../assets/image/tovar-1.svg'
 
-function Card(props) {
+function Card({ title, discount, oldPrice, newItem, price }) {
 
-    let classNameDependsOnDiscount= props.discount === false ? "hidden" : "";
-    let classNameDependsOnOldPrice = props.oldPrice === false ? "hidden" : "";
-    let classNameDependsOnOldNewItem = props.newItem === false ? "hidden" : "";
+    const [fav, setFav] = useState(false)
+    const [plus, setPlus] = useState(false)
+
+    const onClickFav = () => {
+        setFav(!fav);
+    }
+
+    const onClickPlus = () => {
+        setPlus(!plus);
+    }
+
+
+    let classNameDependsOnDiscount = discount === false ? "hidden" : "";
+    let classNameDependsOnOldPrice = oldPrice === false ? "hidden" : "";
+    let classNameDependsOnOldNewItem = newItem === false ? "hidden" : "";
 
     return (
         <div className="short-catalog__item">
-            <a href="src/components/features/home/components/NewCards/Card/index.jsx" className="short-catalog__img-link">
+            <a href="src/components/features/home/components/NewCards/Card/index.jsx"
+               className="short-catalog__img-link">
                 <div className={`short-catalog__lt-info`}>
-                    <span className={`short-catalog__new ${classNameDependsOnOldNewItem}`}>{props.newItem ? 'Новинка' : ''}</span>
+                    <span
+                        className={`short-catalog__new ${classNameDependsOnOldNewItem}`}>{newItem ? 'Новинка' : ''}</span>
                 </div>
                 <div className={`short-catalog__lb-info ${classNameDependsOnDiscount}`}>
-                    <span className={`short-catalog__discount`}>{`-${props.discount}%`}</span>
+                    <span className={`short-catalog__discount`}>{`-${discount}%`}</span>
                 </div>
                 <img src={pictureOfItem} alt="Catalog Img" className="index__catalog-img"/>
             </a>
             <div className="short-catalog__price">
-                <p className="short-catalog__price-num">{`${props.price} ₽`}</p>
-                <s className={`short-catalog__price-s-num ${classNameDependsOnOldPrice}`}>{`${props.oldPrice} ₽`}</s>
+                <p className="short-catalog__price-num">{`${price} ₽`}</p>
+                <s className={`short-catalog__price-s-num ${classNameDependsOnOldPrice}`}>{`${oldPrice} ₽`}</s>
             </div>
             <div className="short-catalog__item-title">
-                <a href="src/components/features/home/components/NewCards/Card/index.jsx" className="short-catalog__item-link">{props.title}</a>
+                <a href="src/components/features/home/components/NewCards/Card/index.jsx"
+                   className="short-catalog__item-link">{title}</a>
             </div>
-            <a className='short-catalog__add-cart' href="src/components/features/home/components/NewCards/Card/index.jsx">В корзину</a>
+            <div>
+                <img width={64} onClick={onClickFav} height={64} src={fav ? `${heartFavActive}` : `${heartFavNeActive}`}
+                     alt="Favorite button"/>
+                <img width={64} height={64} onClick={onClickPlus} src={plus ? `${plusBtnActive}` : `${plusBtnNeActive}`} alt="Plus button"/>
+            </div>
         </div>
     );
 }
--- front/src/components/features/home/components/NewCards/index.jsx
+++ front/src/components/features/home/components/NewCards/index.jsx
@@ -3,6 +3,7 @@ import Card from './Card/index.jsx'
 import {useQuery} from "@tanstack/react-query";
 
 import './mainLayout.scss'
+import Skeleton from "./SkeletonCards/skeleton.jsx";
 
 export default function NewCard() {
 
@@ -14,7 +15,7 @@ export default function NewCard() {
             ),
     })
 
-    if (isLoading) return 'Загрузка...'
+    if (isLoading) return <Skeleton />
 
     if (error) return 'An error has occurred: ' + error.message
 
@@ -24,15 +25,17 @@ export default function NewCard() {
             <h3>Популярный товар</h3>
             <div className="short-catalog__wrapper">
                 {
-                    data.map(item =>
+                    data.map((item, index) =>
                         <Card
-                            key={item.id}
+                            key={index}
                             title={item.title}
                             sale={item.sale}
                             newItem={item.newItem}
                             price={item.price}
                             oldPrice={item.oldPrice}
                             discount={item.discount}
+                            onClickPlus={()=>console.log('Была нажата кнопка добавить')}
+                            onClickFav={()=>console.log('Была нажата кнопка добавить')}
                         />)
                 }
             </div>
