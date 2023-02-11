daf50c77e7f1bd9be3d224aae8895fb1b1c1da82
Renthop <onedostupov@gmail.com>
2023-02-11 10:37:22 +0300

    front(feat): creating new routing

--- front/src/routes/routes.jsx
+++ front/src/routes/routes.jsx
@@ -3,6 +3,9 @@ import Home from "../pages/Home.jsx";
 import ErrorPage from "../pages/Error-page.jsx";
 import Profile from "../pages/Profile.jsx";
 import PageNotFound from "../pages/404.jsx";
+import Cart from "../pages/Cart.jsx";
+import Register from "../pages/Register.jsx";
+import Auth from "../pages/Auth.jsx";
 
 export const router = createBrowserRouter([
     {
@@ -13,6 +16,21 @@ export const router = createBrowserRouter([
     {
         path: "/profile",
         element: <Profile />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/cart",
+        element: <Cart />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/login",
+        element: <Auth />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
         errorElement: <ErrorPage />,
     },
     {
