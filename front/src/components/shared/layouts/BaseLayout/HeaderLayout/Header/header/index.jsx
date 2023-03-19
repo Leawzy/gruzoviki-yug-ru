6ce8b07c66b2f2c7afcf125053b694d40b0e0569
Renthop <onedostupov@gmail.com>
2023-03-19 14:52:03 +0300

    front(feat): creating base logic for search input

--- front/src/components/shared/layouts/BaseLayout/HeaderLayout/Header/header/index.jsx
+++ front/src/components/shared/layouts/BaseLayout/HeaderLayout/Header/header/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
+import { redirect } from "react-router-dom";
 
 import account from '../../../../../../../assets/image/icons/account.svg'
 import basket from '../../../../../../../assets/image/icons/basket.svg'
@@ -8,6 +9,23 @@ import favorite from '../../../../../../../assets/image/icons/favorite.svg'
 import Navbar from "../navbar/index.jsx";
 
 function Header() {
+
+    const [searchQuery, setSearchQuery] = useState('');
+    const [redirectToCatalog, setRedirectToCatalog] = useState(false);
+
+    const handleInputChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        setRedirectToCatalog(true);
+    };
+
+    if (redirectToCatalog) {
+        return redirect(`/catalog?search=${searchQuery}`)
+    }
+
     return (
         <div className='header__center'>
             <div className='container'>
@@ -18,7 +36,8 @@ function Header() {
                     </div>
                     <Navbar />
                     <div className="header__search">
-                        <input className='header__search-input' type="text" placeholder={'Что будем искать?'}/>
+                        <input className='header__search-input' type="text" placeholder={'Что будем искать?'} value={searchQuery}
+                               onChange={handleInputChange}/>
                         <button type={'submit'} className='header__search-submit'></button>
                     </div>
                     <div className="header__contact">
