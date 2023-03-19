4ed971f0318a0b2954afe10d7148e85e6b9e4bba
Iliya <facyoucraft@gmail.com>
2023-03-19 18:43:48 +0300

    feat(back): Add profile controller and func. And edit route

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\V1\ProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\V1\UserController;
@@ -19,6 +20,12 @@ use App\Http\Controllers\API\V1\ProductController;
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
 Route::get('/test', [UserController::class, 'test']);
 Route::get('/product_list', [ProductController::class, 'showProducts']);
 Route::get('/card/{id}', [ProductController::class, 'CardProduct']);
+
+Route::middleware("auth:api")->group(function(){
+    Route::get('/profile', [ProfileController::class, 'show_profile']);
+});
