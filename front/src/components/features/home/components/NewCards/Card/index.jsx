09cbb6d410ed33bd331113af700015807ca08155
Renthop <onedostupov@gmail.com>
2023-03-19 14:52:31 +0300

    front(fix): fixed date from API and changing functions

--- front/src/components/features/home/components/NewCards/Card/index.jsx
+++ front/src/components/features/home/components/NewCards/Card/index.jsx
@@ -6,16 +6,16 @@ import {Link} from "react-router-dom";
 
 function Card(props) {
     const [quantity, setQuantity] = useState(props.quantity);
-    const [isAddedToCart , setIsAddedToCart] = useState(false)
+    const [isAddedToCart, setIsAddedToCart] = useState(false)
 
     const addToCart = async () => {
         try {
-            const res = await axios.post("", {
+            const res = await axios.post("api/cart", {
                 productID: props.id,
                 quantity: 1
             })
             console.log(res)
-            if(res.status === 200) {
+            if (res.status === 200) {
                 setIsAddedToCart(true)
                 setQuantity(quantity - 1)
             }
@@ -28,9 +28,8 @@ function Card(props) {
 
     const removeFromCart = async () => {
         try {
-            const res = await axios.delete("/", {
-                productID: props.id,
-                productName: props.title
+            const res = await axios.delete("api/cart", {
+                productID: props.id
             })
             console.log(res)
             setQuantity(props.quantity)
@@ -44,34 +43,32 @@ function Card(props) {
     }
 
     return (
-        <Link to={`/products/${props.id}`}>
-            <div className="short-catalog__item" id={props.id}>
-                <a href="src/components/features/home/components/NewCards/Card/index.jsx"
-                   className="short-catalog__img-link">
-                    <div className={`short-catalog__lt-info`}>
+        <div className="short-catalog__item" id={props.id}>
+            <Link to={`/products/${props.id}`}
+                  className="short-catalog__img-link">
+                <div className={`short-catalog__lt-info`}>
                     <span
                         className={`short-catalog__new`}>{`Бренд: ${props.brand}`}</span>
-                    </div>
-                    <div className={`short-catalog__lb-info`}>
-                        <span className={`short-catalog__discount`}>{`Количество: ${quantity}`}</span>
-                    </div>
-                    <img src={pictureOfItem} alt="Catalog Img" className="index__catalog-img"/>
-                </a>
-                <div className="short-catalog__price">
-                    <p className="short-catalog__price-num">{`${props.price} ₽`}</p>
                 </div>
-                <div className="short-catalog__item-title">
-                    <a href="src/components/features/home/components/NewCards/Card/index.jsx"
-                       className="short-catalog__item-link">{props.title}</a>
-                    <p>{}</p>
+                <div className={`short-catalog__lb-info`}>
+                    <span className={`short-catalog__discount`}>{`Количество: ${quantity}`}</span>
                 </div>
-                <div>
-                    {quantity > 0 && !isAddedToCart && (<button onClick={addToCart}>Добавить в корзину</button>)}
-                    {quantity === 0 && <p>Товар закончился</p>}
-                    {isAddedToCart && <button onClick={removeFromCart}>Убрать с корзины</button>}
-                </div>
+                <img src={pictureOfItem} alt="Catalog Img" className="index__catalog-img"/>
+            </Link>
+            <div className="short-catalog__price">
+                <p className="short-catalog__price-num">{`${props.price} ₽`}</p>
             </div>
-        </Link>
+            <div className="short-catalog__item-title">
+                <Link to={`/products/${props.id}`}
+                   className="short-catalog__item-link">{props.title}</Link>
+                <p>{props.short_desc}</p>
+            </div>
+            <div>
+                {quantity > 0 && !isAddedToCart && (<button onClick={addToCart}>Добавить в корзину</button>)}
+                {quantity === 0 && <p>Товар закончился</p>}
+                {isAddedToCart && <button onClick={removeFromCart}>Убрать с корзины</button>}
+            </div>
+        </div>
     );
 }
 
