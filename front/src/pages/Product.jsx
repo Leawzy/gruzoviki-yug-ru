f1f3506e9c280324034dce5f36b3c6b98bbf4e43
Renthop <onedostupov@gmail.com>
2023-03-20 00:23:00 +0300

    front(feat): creating new function to get data and render data

--- front/src/pages/Product.jsx
+++ front/src/pages/Product.jsx
@@ -1,38 +1,43 @@
-import React from 'react';
-import { useParams } from "react-router-dom";
-import {useQuery} from "@tanstack/react-query";
-import axios from "axios";
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+function useFetchRepoData(id) {
+    return useQuery({
+        queryKey: ['repoData', id],
+        queryFn: async () => {
+            try {
+                const res = await axios.get(`http://5.167.50.180:8876/api/card/${id}`);
+                return res.data.data;
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+    });
+}
 
 function Product() {
-    const {id} = useParams()
+    const { id } = useParams();
+    const { isLoading, error, data } = useFetchRepoData(id);
 
-    async function fetchRepoData(id) {
-        try {
-            const res = await axios.get(`http://5.167.50.180:8876/api/card/${id}`);
-            return res.data.data;
-        } catch (error) {
-            throw new Error(error);
-        }
-    }
-
-    const { isLoading, error, data } = useQuery({
-        queryKey: ["repoData"],
-        queryFn: () => fetchRepoData(id),
-    });
+    useEffect(() => {
+        document.title = data ? data.title : 'Loading...';
+    }, [data]);
 
     if (isLoading) {
         return <div>Loading...</div>;
     }
 
-    if(error) {
-        return <div>Error: {error}</div>
+    if (error) {
+        return <div>Error: {error.message}</div>;
     }
 
     return (
         <div>
-            <h1></h1>
+            <h1>{data.title}</h1>
             <p>ID: {id}</p>
-            <p>{data.title}</p>
+            <p>{data.short_desc}</p>
         </div>
     );
 }
