547a12a6fb3c5ed91da860e1b708d9f75f37c4d0
Renthop <onedostupov@gmail.com>
2023-03-19 11:10:41 +0300

    front(feat): creating simple layout for footer

--- front/src/components/shared/layouts/BaseLayout/FooterLayout/Footer/index.jsx
+++ front/src/components/shared/layouts/BaseLayout/FooterLayout/Footer/index.jsx
@@ -2,14 +2,76 @@ import React from 'react';
 
 function Footer() {
     return (
-        <footer className={footer}>
-            <div className={container}>
-                <a href="src/components/shared/layouts/BaseLayout/FooterLayout/Footer/index.jsx#" className={footer__logo-link}>
-                    <img src="src/components/shared/layouts/BaseLayout/FooterLayout/Footer/index.jsx" alt="Logo"/>
-                </a>
-                <ul className={footer__nav}>
+        <footer className={'footer'}>
+            <div className={'footer__copyright'}>
+                <div className="container">
+                    <div className="footer__wrapper">
+                        <div className="footer__info">
+                            <a href="#" className={'footer__logo-link'}>
+                                <img src="/" alt="Logo"/>
+                            </a>
+                            <div className={'footer__phone'}>
+                                <a href="tel:">8 989 898 88 23</a>
+                                <a href="tel:">8 989 898 88 23</a>
+                            </div>
+                            <div className={'footer__address'}>
 
-                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <ul className={'footer__nav'}>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Как оформить заказ</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Доставка</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Оплата</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Прием товара</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Почему покупать у нас</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Подключение техники</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Отзывы о нас</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Пользовательское соглашение</a>
+                        </li>
+                    </ul>
+                    <ul className={'footer__nav'}>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Как оформить заказ</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Доставка</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Оплата</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Прием товара</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Почему покупать у нас</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Подключение техники</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Отзывы о нас</a>
+                        </li>
+                        <li className={'footer__nav-link'}>
+                            <a href={'#'}>Пользовательское соглашение</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </footer>
     );
