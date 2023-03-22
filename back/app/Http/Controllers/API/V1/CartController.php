ba7f3a8f5e6285b0cc6d8d8186a5608b5f202b95
Iliya <facyoucraft@gmail.com>
2023-03-22 22:38:29 +0300

    feat(back): add cart controller

---
+++ back/app/Http/Controllers/API/V1/CartController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function cart_order()
+    {
+
+    }
+}
