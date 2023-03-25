3ec393540463346f2eadaff4e1ff4b584ae04dcc
Renthop <onedostupov@gmail.com>
2023-03-25 22:00:25 +0300

    front(feat): Creating render component with dynamic API values

--- front/src/components/features/News/index.jsx
+++ front/src/components/features/News/index.jsx
@@ -1,61 +1,47 @@
 import React from 'react';
 import newsTest from "../../../assets/image/news/remgruzo-01.jpg";
+import {Link} from "react-router-dom";
+import {useNewsList} from "../../../hooks/useFetchHook.js";
+import Skeleton from "../NewCards/SkeletonCards/skeleton.jsx";
 
 function News() {
+    const {isLoading, error, data} = useNewsList();
+    const maxItems = 6;
+
+    if (isLoading) {
+        return <Skeleton/>;
+    }
+
+    if (error) {
+        return <div>An error has occurred: {error.message}</div>;
+    }
+
+
     return (
         <section className='news'>
             <div className="news__wrapper">
                 <h3>Новости</h3>
             </div>
             <div className="news__item-list">
-                <div className="news__item">
-                    <a href="#" className='news__link'>
-                        <img className="news__link-img" src={newsTest} alt="News Pic"/>
-                        <div className="news__link-bg">
-                            <span className="news__title">Title</span>
-                        </div>
-                    </a>
-                </div>
-                <div className="news__item">
-                    <a href="#" className='news__link'>
-                        <img className="news__link-img" src={newsTest} alt="News Pic"/>
-                        <div className="news__link-bg">
-                            <span className="news__title">Title</span>
-                        </div>
-                    </a>
-                </div>
-                <div className="news__item">
-                    <a href="#" className='news__link'>
-                        <img className="news__link-img" src={newsTest} alt="News Pic"/>
-                        <div className="news__link-bg">
-                            <span className="news__title">Title</span>
-                        </div>
-                    </a>
-                </div>
-                <div className="news__item">
-                    <a href="#" className='news__link'>
-                        <img className="news__link-img" src={newsTest} alt="News Pic"/>
-                        <div className="news__link-bg">
-                            <span className="news__title">Title</span>
-                        </div>
-                    </a>
-                </div>
-                <div className="news__item">
-                    <a href="#" className='news__link'>
-                        <img className="news__link-img" src={newsTest} alt="News Pic"/>
-                        <div className="news__link-bg">
-                            <span className="news__title">Title</span>
-                        </div>
-                    </a>
-                </div>
-                <div className="news__item">
-                    <a href="#" className='news__link'>
-                        <img className="news__link-img" src={newsTest} alt="News Pic"/>
-                        <div className="news__link-bg">
-                            <span className="news__title">Title</span>
-                        </div>
-                    </a>
-                </div>
+                {
+                    data.slice(0, maxItems).map((item, index) => {
+                        const formattedDate = new Date(item.created_at).toLocaleString('ru-RU', {
+                            dateStyle: 'full',
+                        });
+                        return (
+                            <div className="news__item news__card" key={index}>
+                                <Link to={`/news/${item.id}`} className='news__link'>
+                                    <img className="news__link-img" src={newsTest} alt="News Pic"/>
+                                    <div className="news__link-bg">
+                                        <span className="news__title">{item.title}</span>
+                                        <span className="news__desc">{item.short_desc}</span>
+                                        <span className="news__time">{formattedDate}</span>
+                                    </div>
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </section>
     );
