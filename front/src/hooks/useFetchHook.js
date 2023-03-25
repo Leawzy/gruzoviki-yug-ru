f1192d99a450b2d7d0fd1b4898ac2b2b72188d2b
Renthop <onedostupov@gmail.com>
2023-03-25 21:57:24 +0300

    front(feat): Add new function Hook

--- front/src/hooks/useFetchHook.js
+++ front/src/hooks/useFetchHook.js
@@ -23,4 +23,30 @@ function useProductList() {
     return { isLoading, error, data };
 }
 
-export default useProductList;
+function useNewsList() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function fetchNewsList() {
+            setIsLoading(true);
+            try {
+                const res = await axios.get('http://5.167.50.180:8876/api/posts/', {
+                    params: {
+                        limit: 5
+                    }
+                });
+                setData(res.data.data);
+            } catch (error) {
+                setError(error);
+            }
+            setIsLoading(false);
+        }
+        fetchNewsList();
+    }, []);
+
+    return { isLoading, error, data };
+}
+
+export {useProductList, useNewsList};
