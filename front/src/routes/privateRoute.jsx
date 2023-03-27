a9bc05969de2f08b4b236541f5fe157e4ff3ac36
Renthop <onedostupov@gmail.com>
2023-03-27 18:04:55 +0300

    front(feat): Creating routes for admin

--- front/src/routes/privateRoute.jsx
+++ front/src/routes/privateRoute.jsx
@@ -4,16 +4,23 @@ import {Navigate, Outlet} from "react-router-dom";
 function PrivateAuthRoute() {
     const auth = Cookies.get('api_token');
 
-    return(auth ? <Outlet /> : <Navigate to={'/login'} />);
+    return (auth ? <Outlet/> : <Navigate to={'/login'}/>);
 }
 
 function PrivateNoNAuthRoute() {
     const auth = Cookies.get('api_token');
+    const authAdmin = Cookies.get('admin_token');
 
     return (
-        auth ? <Navigate to={'/'} /> : <Outlet />
+        auth || authAdmin ? <Navigate to={'/'}/> : <Outlet/>
     )
 }
 
+function AdminRoute() {
+    const auth = Cookies.get('admin_token');
 
-export {PrivateAuthRoute, PrivateNoNAuthRoute}
+    return (auth ? <Outlet/> : <Navigate to={'/'}/>);
+}
+
+
+export {PrivateAuthRoute, PrivateNoNAuthRoute, AdminRoute}
