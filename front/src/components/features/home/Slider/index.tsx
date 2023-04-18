df16164a92383715490be1c699275c42bd5141ac
Renth0p <44197289+Renth0p@users.noreply.github.com>
2023-04-18 12:55:00 +0300

    feat(feat): creating Slider component

---
+++ front/src/components/features/home/Slider/index.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+import cn from './style.module.scss';
+
+interface SliderIF {
+    images: string[];
+}
+
+export default function Slider({ images }: SliderIF) {
+    const [index, setIndex] = useState(0);
+
+    const handleClick = (direction: string) => {
+        if (direction === 'prev') {
+            setIndex(index === 0 ? images.length - 1 : index - 1);
+        } else if (direction === 'next') {
+            setIndex(index === images.length - 1 ? 0 : index + 1);
+        }
+    };
+
+    return (
+        <div className={cn.imageSlider}>
+            <button className={cn.prevButton} onClick={() => handleClick('prev')}>
+                &lt;
+            </button>
+            <Image
+                className={cn.sliderImage}
+                width={1200}
+                height={200}
+                src={images[index]}
+                alt={`Image ${index + 1}`}
+            />
+            <button className={cn.nextButton} onClick={() => handleClick('next')}>
+                &gt;
+            </button>
+        </div>
+    );
+}
