7150417e41b63c9e53fe4ff5ce39188c70e5a71e
Renth0p <44197289+Renth0p@users.noreply.github.com>
2023-04-24 16:59:18 +0300

    feat(fix): Fixed errors

--- front/src/hooks/usePopularCardHook.ts
+++ front/src/hooks/usePopularCardHook.ts
@@ -1,22 +1,28 @@
 import { useEffect, useState } from 'react';
 
 import { apiFetch } from '../axios/global';
+import { Product } from '../types/ProductType';
 
 function useProductList() {
-    const [productList, setProductList] = useState([]);
+    const [productList, setProductList] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
         async function fetchProductList() {
             try {
-                const res = await apiFetch('/api/product_list', { method: 'get' });
+                const res: { data: { data: Product[] } } = await apiFetch('/api/product_list', {
+                    method: 'get',
+                });
                 setProductList(res.data.data);
+                setLoading(true);
             } catch (e) {
                 console.error(e);
+                setLoading(false);
             }
         }
-        fetchProductList();
+        fetchProductList().catch(e => console.error(e));
     }, []);
 
-    return { productList };
+    return { productList, loading };
 }
 
 export { useProductList };
