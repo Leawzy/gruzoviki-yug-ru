9b840730041e91b9c786a3e7c44dd5086ab6a92f
Renth0p <44197289+Renth0p@users.noreply.github.com>
2023-04-24 17:02:44 +0300

    feat(feat): Creating Provider for Cart

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -7,11 +7,16 @@ import { DefaultSeo } from 'next-seo';
 import SEO from 'next-seo.config';
 import React, { useEffect, useState } from 'react';
 
+import { CartStore } from '../mobx/CartStore/cart';
+import { CartStoreProvider } from '../mobx/CartStore/CartStoreContext';
+
 if (typeof window !== 'undefined') {
     React.useLayoutEffect = useEffect;
 }
 
 type DehydratedProps = { dehydratedState: DehydratedState };
+
+const cartStore = new CartStore();
 
 function MyApp({ Component, pageProps }: AppProps<DehydratedProps>) {
     const [queryClient] = useState(
@@ -26,13 +31,16 @@ function MyApp({ Component, pageProps }: AppProps<DehydratedProps>) {
     );
 
     return (
-        <QueryClientProvider client={queryClient}>
-            <Hydrate state={pageProps.dehydratedState}>
-                <DefaultSeo {...SEO} />
-                <Component {...pageProps} />
-            </Hydrate>
-            <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-        </QueryClientProvider>
+        // @ts-ignore
+        <CartStoreProvider value={cartStore}>
+            <QueryClientProvider client={queryClient}>
+                <Hydrate state={pageProps.dehydratedState}>
+                    <DefaultSeo {...SEO} />
+                    <Component {...pageProps} />
+                </Hydrate>
+                <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+            </QueryClientProvider>
+        </CartStoreProvider>
     );
 }
 
