44651069aa0f499dbdb205553be951fa41a51a74
Renth0p <44197289+Renth0p@users.noreply.github.com>
2023-04-25 17:27:44 +0300

    feat(front): Connect styles for lib

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/index.scss';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { DehydratedState, Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
