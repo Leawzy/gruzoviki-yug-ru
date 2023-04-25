2f7849d31f2b6dee5653f03341a76a519f0eb92e
Renth0p <44197289+Renth0p@users.noreply.github.com>
2023-04-25 16:16:46 +0300

    fix(front): Fix problem with API get

--- front/src/hooks/useNewsHook.ts
+++ front/src/hooks/useNewsHook.ts
@@ -6,23 +6,25 @@ import { News } from '../types/NewsType';
 function useNewsList() {
     const [newsList, setNewsList] = useState<News[]>([]);
     const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        async function fetchProductList() {
-            try {
-                const res: { data: { data: News[] } } = await apiFetch('/api/posts', {
-                    method: 'get',
-                });
-                setNewsList(res.data.data);
-                setLoading(true);
-            } catch (e) {
-                console.error(e);
-                setLoading(false);
-            }
+
+    async function fetchNewsList() {
+        try {
+            const res: { data: { data: News[] } } = await apiFetch('/api/posts', {
+                method: 'get',
+            });
+            setNewsList(res.data.data);
+            setLoading(true);
+        } catch (e) {
+            console.error(e);
+            setLoading(false);
         }
-        fetchProductList().catch(e => console.error(e));
+    }
+
+    useEffect(() => {
+        fetchNewsList().catch(e => console.error(e));
     }, []);
 
-    return { newsList, loading };
+    return { newsList, loading, fetchNewsList };
 }
 
 export { useNewsList };
