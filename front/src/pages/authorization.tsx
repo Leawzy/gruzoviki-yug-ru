b56364b493be9b30d19f6622d01860e3fb1af83d
Renth0p <44197289+Renth0p@users.noreply.github.com>
2023-04-25 17:26:18 +0300

    feat(front): Creating empty preparing pages of Register and Login

---
+++ front/src/pages/authorization.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AuthorizationPage() {
+    return <div />;
+}
+
+export default AuthorizationPage;
---
+++ front/src/pages/registration.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RegistrationPage() {
+    return <div />;
+}
+
+export default RegistrationPage;
