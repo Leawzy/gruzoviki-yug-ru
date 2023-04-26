23beddf311d7d1573b10b0e816e12b44fadec00b
Iliya <facyoucraft@gmail.com>
2023-04-26 15:37:16 +0300

    feat(back): add Swagger

--- back/app/Http/Controllers/Controller.php
+++ back/app/Http/Controllers/Controller.php
@@ -6,6 +6,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     title="Gruzoviki-Yug API documentaion",
+ *     version="1.0"
+ * )
+ * @OA\Server(
+ *     description="Gruzoviki-Yug server",
+ *     url="http://127.0.0.1:8000/api/"
+ * )
+ * @OA\SecurityScheme(
+ *     type="apiKey",
+ *     in="header",
+ *     name="Authorization",
+ *     securityScheme="authorization"
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
