edede5d41e847958dc939fcd576d22ba4b879584
Iliya <facyoucraft@gmail.com>
2023-04-27 14:03:54 +0300

    feat(back): add login func

--- back/app/Http/Controllers/Api/v1/UserController.php
+++ back/app/Http/Controllers/Api/v1/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\v1;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserController extends Controller
@@ -29,6 +30,26 @@ class UserController extends Controller
 
         $token = JWTAuth::fromUser($user);
 
+        return response()->json(compact('token'));
+    }
+
+    public function login(Request $request)
+    {
+        // Получаем email и пароль из запроса
+        $credentials = $request->only('email', 'password');
+
+        try {
+            // Проверяем email и пароль пользователя
+            if (!$token = JWTAuth::attempt($credentials)) {
+                // Если email или пароль неверны, возвращаем ошибку
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // Если не удалось создать токен, возвращаем ошибку
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+
+        // Если проверка прошла успешно, возвращаем токен
         return response()->json(compact('token'));
     }
 }
