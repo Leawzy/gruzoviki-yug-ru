344e79fd10dad6f06b47fb2b2e56513616bbf954
Iliya <facyoucraft@gmail.com>
2023-04-27 14:05:32 +0300

    feat(back): add new route

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\OtherController;
 use App\Http\Controllers\Api\v1\ProductController;
 use App\Http\Controllers\Api\v1\UserController;
 use Illuminate\Http\Request;
@@ -15,15 +16,25 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+//Product function
 Route::get('/product_list', [ProductController::class, 'showProducts']);
-Route::get('/card/{id}', [ProductController::class, 'showCardProduct']);
-Route::post('/register', [UserController::class, 'register']);
+Route::get('/card/{id}', [ProductController::class, 'getCardProduct']);
 
-Route::middleware(['auth:api', 'admin'])->group(function () {
-    Route::get('/card/{id}', [ProductController::class, 'showCardProduct']);
+//User function
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
+// Other Function
+Route::get('/slider', [OtherController::class, 'showSlider']);
+Route::get('/posts', [OtherController::class, 'showPost']);
+Route::get('/post/{id}', [OtherController::class, 'getPostById']);
+
+//Auth User function
+Route::middleware(['auth:api'])->group(function () {
+
 });
 
-Route::middleware(['auth:api'])->group(function () {
-    Route::get('/product_list', [ProductController::class, 'showProducts']);
+//Admin function
+Route::middleware(['auth:api', 'admin'])->group(function () {
+
 });
