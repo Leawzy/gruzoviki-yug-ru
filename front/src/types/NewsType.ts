06277af1194b168542bcffcbbeffb6a5a197129c
Renth0p <44197289+Renth0p@users.noreply.github.com>
2023-04-27 21:09:56 +0300

    remove(front): Removing not using interface

--- front/src/types/NewsType.ts
+++ front/src/types/NewsType.ts
@@ -1,16 +1,7 @@
 export interface News {
-    created_at: number | string;
+    createdAt: number | string;
     id: number | string;
     img: string;
     title: string;
     short_desc: string;
-}
-
-export interface NewsPost {
-    description: string;
-    img: string;
-    short_desc: string;
-    title: string;
-    slug: string;
-    created_at: number | string;
 }
