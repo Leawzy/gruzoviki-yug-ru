b29c69d95522911feb64a139b4903c4546f0b597
Iliya <facyoucraft@gmail.com>
2023-04-28 12:23:34 +0300

    feat(back): add Swagger

--- back/app/Http/Controllers/Api/v1/ProfileController.php
+++ back/app/Http/Controllers/Api/v1/ProfileController.php
@@ -9,6 +9,46 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class ProfileController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/profile",
+     *     security={{"bearerAuth":{}}},
+     *     operationId="getUserProfile",
+     *     tags={"Profile"},
+     *     summary="Get user profile",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="object",
+     *                 @OA\Property(type="integer", property="id"),
+     *                 @OA\Property(type="string", property="firstName"),
+     *                 @OA\Property(type="string", property="lastName"),
+     *                 @OA\Property(type="string", property="email"),
+     *                 @OA\Property(type="string", property="phoneNumber"),
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized",
+     *         @OA\JsonContent(
+     *             @OA\Property(type="boolean", property="success", example=false),
+     *             @OA\Property(type="string", property="error", example="Unauthorized")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="User not found",
+     *         @OA\JsonContent(
+     *             @OA\Property(type="boolean", property="success", example=false),
+     *             @OA\Property(type="string", property="error", example="User not found")
+     *         )
+     *     )
+     * )
+     */
     public function getUserProfile()
     {
         $token = JWTAuth::parseToken();
@@ -21,6 +61,54 @@ class ProfileController extends Controller
         ];
     }
 
+    /**
+     * @OA\Patch(
+     *     path="/profile/change/password",
+     *     summary="Change user password",
+     *     description="Changes the password of the authenticated user.",
+     *     operationId="changePassword",
+     *     tags={"Profile"},
+     *     security={{ "bearerAuth": {} }},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="oldPassword",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="newPassword",
+     *                     type="string"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Password changed successfully.",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Password changed successfully."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="Invalid credentials.",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Invalid credentials."
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function changeUserPassword(Request $request)
     {
         $request->validate([
@@ -49,6 +137,70 @@ class ProfileController extends Controller
         }
     }
 
+    /**
+     * @OA\Patch(
+     *     path="/profile/change/info",
+     *     summary="Updates user profile data",
+     *     tags={"Profile"},
+     *     operationId="changeInfo",
+     *     security={{"bearerAuth":{}}},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/x-www-form-urlencoded",
+     *             @OA\Schema(
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="firstName",
+     *                     description="User's first name",
+     *                     type="string",
+     *                     example="John"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="lastName",
+     *                     description="User's last name",
+     *                     type="string",
+     *                     example="Doe"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="email",
+     *                     description="User's email address",
+     *                     type="string",
+     *                     example="johndoe@example.com"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="phone_number",
+     *                     description="User's phone number",
+     *                     type="string",
+     *                     example="+1 (555) 123-4567"
+     *                 ),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="User profile data updated successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Данные успешно обновлены"
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Failed to update user profile data",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Ошибка"
+     *             ),
+     *         ),
+     *     ),
+     * )
+     */
     public function updateUserData(Request $request)
     {
         $token = JWTAuth::parseToken();
--- back/app/Http/Controllers/Api/v1/UserController.php
+++ back/app/Http/Controllers/Api/v1/UserController.php
@@ -10,6 +10,43 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserController extends Controller
 {
+
+    /**
+     * @OA\Post(
+     *     path="/register",
+     *     summary="Create a new user",
+     *     tags={"Users"},
+     *     operationId="createUser",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(property="firstName", type="string", example="John"),
+     *                 @OA\Property(property="lastName", type="string", example="Doe"),
+     *                 @OA\Property(property="email", type="string", format="email", example="johndoe@example.com"),
+     *                 @OA\Property(property="password", type="string", example="password123"),
+     *                 @OA\Property(property="password_confirmation", type="string", example="password123"),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="token", type="string", example="JWT Token"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad Request",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="success", type="boolean", example=false),
+     *             @OA\Property(property="error", type="array", @OA\Items(type="string", example="The given data was invalid.")),
+     *         ),
+     *     ),
+     * )
+     */
     public function createUser(Request $request): \Illuminate\Http\JsonResponse
     {
 
@@ -33,6 +70,104 @@ class UserController extends Controller
         return response()->json(compact('token'));
     }
 
+    /**
+     * @OA\Post(
+     *     path="/login",
+     *     summary="Authenticate user",
+     *     operationId="authUser",
+     *     tags={"Users"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="JSON payload with email and password",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 required={"email", "password"},
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string",
+     *                     format="email",
+     *                     description="Email address of the user"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     type="string",
+     *                     description="Password of the user"
+     *                 ),
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful authentication",
+     *         @OA\JsonContent(
+     *             required={"success", "token"},
+     *             @OA\Property(
+     *                 property="success",
+     *                 type="boolean",
+     *                 description="Status of successful authentication"
+     *             ),
+     *             @OA\Property(
+     *                 property="token",
+     *                 type="string",
+     *                 description="API token of the user"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Invalid credentials",
+     *         @OA\JsonContent(
+     *             required={"error"},
+     *             @OA\Property(
+     *                 property="error",
+     *                 type="string",
+     *                 description="Invalid credentials"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request",
+     *         @OA\JsonContent(
+     *             required={"success", "errors"},
+     *             @OA\Property(
+     *                 property="success",
+     *                 type="boolean",
+     *                 description="Status of successful authentication"
+     *             ),
+     *             @OA\Property(
+     *                 property="errors",
+     *                 type="array",
+     *                 description="Array of errors",
+     *                 @OA\Items(
+     *                     type="string"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Bad request",
+     *         @OA\JsonContent(
+     *             required={"success", "errors"},
+     *             @OA\Property(
+     *                 property="success",
+     *                 type="boolean",
+     *                 description="Could not create token"
+     *             ),
+     *             @OA\Property(
+     *                 property="errors",
+     *                 type="array",
+     *                 description="Array of errors",
+     *                 @OA\Items(
+     *                     type="string"
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function authUser(Request $request)
     {
         // Получаем email и пароль из запроса
