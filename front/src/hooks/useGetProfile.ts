7d9eeb34a705fe8e09fbddaa23af5d1976426a5d
Renth0p <44197289+Renth0p@users.noreply.github.com>
2023-04-28 13:15:34 +0300

    fix(front): Fixed errors

--- front/src/hooks/useGetProfile.ts
+++ front/src/hooks/useGetProfile.ts
@@ -7,14 +7,17 @@ import { ProfileType } from '../types/ProfileType';
 
 function useProfileData() {
     const router = useRouter();
-    const [profile, setProfile] = useState<ProfileType[]>([]);
+    // @ts-ignore
+    const [profile, setProfile] = useState<ProfileType>([]);
     useEffect(() => {
         const getProfile = async () => {
             setAuthToken();
             try {
                 const res: { data: ProfileType[] } = await apiFetch.get('api/profile');
+                // @ts-ignore
                 setProfile(res.data);
             } catch (error) {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
                 if (error.response?.status === 401) {
                     await router.push('/');
                     destroyCookie(null, 'token');
