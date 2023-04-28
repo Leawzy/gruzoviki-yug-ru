6dc850cb9bdb32ad9f641ae14c9f28c3aef44180
Renth0p <44197289+Renth0p@users.noreply.github.com>
2023-04-28 12:31:12 +0300

    feat(front): Add new styles and function to remember user

--- front/src/components/core/forms/AuthForm/index.tsx
+++ front/src/components/core/forms/AuthForm/index.tsx
@@ -1,19 +1,22 @@
 import { useRouter } from 'next/router';
 import { setCookie } from 'nookies';
-import React from 'react';
+import React, { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
-import { apiFetch } from '../../../../axios/global';
+import { apiFetch, dayOfLiveToken } from '../../../../axios/global';
 import cn from '../style.module.scss';
-
-const daysLive = 30 * 24 * 60 * 60;
 
 type Inputs = {
     email: string;
     password: string;
 };
 
+interface ResponseData {
+    token: string;
+}
+
 export default function AuthorizationForm() {
+    const [agreement, setAgreement] = useState(false);
     const router = useRouter();
     const {
         register,
@@ -21,7 +24,7 @@ export default function AuthorizationForm() {
         formState: { errors },
     } = useForm<Inputs>();
     const onSubmit: SubmitHandler<Inputs> = async data => {
-        const res = await apiFetch('api/login', {
+        const res: { status: number; data: ResponseData } = await apiFetch('api/login', {
             method: 'post',
             data: {
                 email: data.email,
@@ -30,20 +33,31 @@ export default function AuthorizationForm() {
         });
         if (res.status === 200) {
             const { token } = res.data;
-            setCookie(null, 'token', token, {
-                maxAge: daysLive,
-            });
+            if (!agreement) {
+                setCookie(null, 'token', token, {
+                    maxAge: dayOfLiveToken(),
+                });
+            } else {
+                setCookie(null, 'token', token, {
+                    maxAge: 7 * 24 * 60 * 60,
+                });
+            }
             await router.push('/');
         }
     };
 
+    function saveMe(e: React.ChangeEvent<HTMLInputElement>) {
+        setAgreement(e.target.checked);
+    }
+
     return (
-        <div className={cn.BackgroundForms}>
+        <div className={cn.AuthForm}>
             <form className={cn.Form} onSubmit={handleSubmit(onSubmit)}>
                 <h1>Авторизация</h1>
                 <label className={cn.FormLabel}>
                     E-mail
                     <input
+                        className={errors.email ? `${cn.errorInput}` : ''}
                         type="email"
                         placeholder="Введите свой e-mail"
                         {...register('email', { required: true })}
@@ -55,6 +69,7 @@ export default function AuthorizationForm() {
                 <label className={cn.FormLabel}>
                     Пароль
                     <input
+                        className={errors.password ? `${cn.errorInput}` : ''}
                         type="password"
                         placeholder="Введите пароль"
                         {...register('password', { required: true })}
@@ -63,7 +78,21 @@ export default function AuthorizationForm() {
                         <span className={cn.FormLabelError}>Это поле должно быть заполненым</span>
                     )}
                 </label>
-                <input type="submit" />
+                {errors.email || errors.password ? (
+                    <div className={cn.FormBtn}>
+                        <input disabled type="submit" value="Авторизироваться" />
+                    </div>
+                ) : (
+                    <div className={cn.FormBtn}>
+                        <input type="submit" value="Авторизироваться" />
+                    </div>
+                )}
+                <div>
+                    <p className="check__auth">
+                        <input type="checkbox" onChange={saveMe} />
+                        Запомнить меня
+                    </p>
+                </div>
             </form>
         </div>
     );
