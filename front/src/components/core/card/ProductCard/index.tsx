70d8f513aa22064d9a68142d250ccc2d15917906
Renth0p <44197289+Renth0p@users.noreply.github.com>
2023-04-28 17:44:29 +0300

    fix(front): Fixed error

--- front/src/components/core/card/ProductCard/index.tsx
+++ front/src/components/core/card/ProductCard/index.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React, { useEffect } from 'react';
 
-import { useCartStore } from '../../../../../mobx/CartStore/CartStoreContext';
-import { Product } from '../../../../../types/ProductType';
-import ButtonAdd from '../../../../core/buttons/ButtonAdd';
-import ButtonRemove from '../../../../core/buttons/ButtonRemove';
+import { useCartStore } from '../../../../mobx/CartStore/CartStoreContext';
+import { Product } from '../../../../types/ProductType';
+import ButtonAdd from '../../buttons/ButtonAdd';
+import ButtonRemove from '../../buttons/ButtonRemove';
 import cn from './style.module.scss';
 
-export default function PopularCard({
+export default function ProductCard({
     id,
     brand,
     quantity,
@@ -21,11 +21,9 @@ export default function PopularCard({
     const store = useCartStore();
 
     useEffect(() => {
-        const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
-        const item = cartItems.find((item: any) => item.id === id);
-        console.log(item);
+        const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]') as Product[];
+        const item = cartItems.find((item: Product) => item.id === id);
         if (item) {
-            console.log(1);
             setAddedToCart(true);
         }
     }, [id]);
