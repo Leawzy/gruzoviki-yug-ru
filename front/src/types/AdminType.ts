fad449fb35c68343f7da8e2164714cb48acec2ed
Renth0p <44197289+Renth0p@users.noreply.github.com>
2023-04-29 23:27:10 +0300

    feat(front): Creating type for admin props

---
+++ front/src/types/AdminType.ts
@@ -0,0 +1,13 @@
+export interface AdminTypeUser {
+    total: string | number;
+    users: {
+        total: string | number;
+    };
+}
+
+export interface AdminTypeBrand {
+    total: string | number;
+    users: {
+        total: string | number;
+    };
+}
