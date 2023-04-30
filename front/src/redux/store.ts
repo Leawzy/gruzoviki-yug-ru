97e3fe46bb8e1c8d26d1891c78a3deb1ab0f3697
Renth0p <44197289+Renth0p@users.noreply.github.com>
2023-04-30 15:14:00 +0300

    fix(front): Creating redux logic for cart, add item, remove item

---
+++ front/src/redux/store.ts
@@ -0,0 +1,11 @@
+import { combineReducers, createStore } from 'redux';
+
+import cartReducer from './cartReducer';
+
+const rootReducer = combineReducers({
+    cart: cartReducer,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
