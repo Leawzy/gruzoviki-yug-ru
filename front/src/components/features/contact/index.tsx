210dda95a3c584c7d227abae22d69562123724de
Renth0p <44197289+Renth0p@users.noreply.github.com>
2023-04-30 19:19:12 +0300

    feat(front): Creating feature-component Contact

---
+++ front/src/components/features/contact/index.tsx
@@ -0,0 +1,110 @@
+import Image from 'next/image';
+import React from 'react';
+
+import calendar from '../../../../public/images/icons/contact/calendar.svg';
+import call from '../../../../public/images/icons/contact/call.svg';
+import invoices from '../../../../public/images/icons/contact/invoices.svg';
+import mail from '../../../../public/images/icons/contact/mail.svg';
+import cn from './style.module.scss';
+
+export default function Contact() {
+    return (
+        <section className={cn.contactPage}>
+            <div className={cn.contactPageLists}>
+                <h1>Контакты</h1>
+                <ul className={cn.contactPageList}>
+                    <div className={cn.contactPageListTop}>
+                        <Image
+                            width={25}
+                            height={25}
+                            src={calendar as string}
+                            alt="Icon in Contact"
+                        />
+                        <p>График работы отдела продаж</p>
+                    </div>
+                    <div className={cn.contactPageListContent}>
+                        <li>9:00 - 22:00 (пн-пт)</li>
+                        <li>Субботу - Воскресенье выходной</li>
+                    </div>
+                    <p>Оформление заказов через сайт круглосуточно</p>
+                </ul>
+                <ul className={cn.contactPageList}>
+                    <div className={cn.contactPageListTop}>
+                        <Image width={25} height={25} src={call as string} alt="Icon in Contact" />
+                        <p>График работы отдела продаж</p>
+                    </div>
+                    <div className={cn.contactPageListContent}>
+                        <li>7 000 000 00 00</li>
+                        <li>7 000 000 00 00</li>
+                        <li>7 000 000 00 00</li>
+                    </div>
+                </ul>
+                <ul className={cn.contactPageList}>
+                    <div className={cn.contactPageListTop}>
+                        <Image width={25} height={25} src={mail as string} alt="Icon in Contact" />
+                        <p>График работы отдела продаж</p>
+                    </div>
+                    <div className={cn.contactPageListContent}>
+                        <li>info@elecity.ru</li>
+                    </div>
+                </ul>
+                <ul className={cn.contactPageList}>
+                    <div className={cn.contactPageListTop}>
+                        <Image
+                            width={25}
+                            height={25}
+                            src={invoices as string}
+                            alt="Icon in Contact"
+                        />
+                        <p>График работы отдела продаж</p>
+                    </div>
+                    <div className={cn.contactPageListContent}>
+                        <li>
+                            Для покупки по безналичному расчету отправляйте запрос на bn@elecity.ru.
+                        </li>
+                    </div>
+                </ul>
+            </div>
+            <div className={cn.contactPageCallBack}>
+                <h2>Оставить обращение</h2>
+                <form className={cn.contactPageForm}>
+                    <div className={cn.contactPageFormTop}>
+                        <label>
+                            ФИО
+                            <input type="text" />
+                        </label>
+                        <label>
+                            Телефон
+                            <input type="text" />
+                        </label>
+                        <label>
+                            Почта
+                            <input type="text" />
+                        </label>
+                        <label>
+                            Выберите отдел
+                            <select>
+                                <option value="1">1</option>
+                                <option value="1">3</option>
+                                <option value="1">2</option>
+                                <option value="1">4</option>
+                            </select>
+                        </label>
+                    </div>
+                    <div className={cn.contactPageFormBottom}>
+                        <textarea />
+                        <button>Отправить</button>
+                    </div>
+                </form>
+            </div>
+            <div className={cn.contactPageMap}>
+                <iframe
+                    src="https://yandex.ru/map-widget/v1/?um=constructor%3Aadf503c8d493db84b041a6d7eb227197f147588a3976d525bfd0865b492dab34&amp;source=constructor"
+                    width="1280"
+                    height="556"
+                    frameBorder="0"
+                />
+            </div>
+        </section>
+    );
+}
