6f13adc2e4ad5cc9acdebdecd357bdd7f60fce69
Renth0p <onedostupov@gmail.com>
2023-05-10 22:48:10 +0300

    feat(front): Add new functions

--- front/src/pages/admin/brand/brandChange.tsx
+++ front/src/pages/admin/brand/brandChange.tsx
@@ -9,13 +9,20 @@ import {
 } from '@mui/x-data-grid';
 import React, { useEffect, useState } from 'react';
 
+import { adminFetch, setAuthToken } from '../../../axios/global';
 import AdminLayout from '../../../components/shared/layouts/AdminLayout';
-import { useAdminBrandData } from '../../../hooks/useAdminHook';
+import { useGetBrandHook } from '../../../hooks/useGetBrandHook';
 import { withAuth } from '../../../utils/withAuth';
 import { withAuthAdmin } from '../../../utils/withAuthAdmin';
 
+interface BrandIF {
+    id: number;
+    title: string;
+    img: string;
+}
+
 function BrandChange() {
-    const { brand } = useAdminBrandData();
+    const { brand } = useGetBrandHook();
     const [rows, setRows] = useState<GridRowsProp>([]);
     const [selectedRow, setSelectedRow] = useState<GridValidRowModel | undefined>([]);
     const [openModal, setOpenModal] = useState(false);
@@ -26,27 +33,62 @@ function BrandChange() {
         setOpenModal(true);
     };
 
+    const handleDeleteRow = (id: GridRowId) => {
+        const selectedRow = rows.find(row => row.id === id);
+    };
+
     const handleCloseModal = () => {
         setOpenModal(false);
     };
 
-    const handleSaveChanges = () => {
-        handleCloseModal();
+    const handleSaveChanges = async () => {
+        setAuthToken();
+        try {
+            // @ts-ignore
+            const { id, title, img }: BrandIF = selectedRow || {};
+            const data = {
+                id,
+                title,
+                img,
+            } as BrandIF;
+            const res: Response = await adminFetch('/brand/change', {
+                method: 'post',
+                data,
+            });
+            if (res.status === 200) {
+                handleCloseModal();
+            }
+        } catch (e) {
+            console.error(e);
+        }
     };
 
     const columns: GridColDef[] = [
-        { field: 'id' },
-        { field: 'title', headerName: 'Название', width: 150 },
+        { field: 'id', width: 20 },
+        { field: 'title', headerName: 'Название', width: 250 },
         {
             field: 'actions',
             headerName: 'Действие',
-            width: 150,
+            headerAlign: 'center',
+            align: 'center',
+            width: 300,
             renderCell: params => {
                 const handleEdit = () => {
                     handleOpenModal(params.id);
                 };
 
-                return <Button onClick={handleEdit}>Изменить</Button>;
+                const handleDelete = () => {
+                    handleDeleteRow(params.id);
+                };
+
+                return (
+                    <>
+                        <Button onClick={handleEdit}>Изменить</Button>
+                        <Button onClick={handleDelete} color="error">
+                            Удалить
+                        </Button>
+                    </>
+                );
             },
         },
     ];
@@ -55,13 +97,14 @@ function BrandChange() {
         const formattedRows: GridRowsProp = brand.map(item => ({
             id: item.id,
             title: item.title,
+            img: item.img,
         }));
         setRows(formattedRows);
     }, [brand]);
 
     return (
         <AdminLayout>
-            <div style={{ height: 700, width: '100%' }}>
+            <div style={{ height: 900, width: '100%' }}>
                 <DataGrid
                     localeText={ruRU.components.MuiDataGrid.defaultProps.localeText}
                     rows={rows}
@@ -76,6 +119,7 @@ function BrandChange() {
                                 setSelectedRow(prev => ({ ...prev, title: e.target.value }));
                             }}
                         />
+                        <img src={selectedRow?.img as string} alt={selectedRow?.title as string} />
                     </DialogContent>
                     <DialogActions>
                         <Button onClick={handleCloseModal}>Отмена</Button>
