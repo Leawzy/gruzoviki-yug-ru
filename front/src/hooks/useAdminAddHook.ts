5e853fcab15821f175f122cbefc2ceccd21575b9
Renth0p <onedostupov@gmail.com>
2023-05-10 22:52:14 +0300

    feat(front): Creating new custom hooks for admin

---
+++ front/src/hooks/useAdminAddHook.ts
@@ -0,0 +1,77 @@
+import { useRouter } from 'next/router';
+import React, { useCallback, useState } from 'react';
+import { toast } from 'react-toastify';
+
+import { adminFetch, setAuthToken } from '../axios/global';
+
+export function useAdminAddHook(url: string) {
+    const [title, setTitle] = useState('');
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const route = useRouter();
+
+    const cancelCreate = useCallback(async () => {
+        await route.replace('/admin/admin');
+    }, [route]);
+
+    const handleFileChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            if (file.type === 'image/webp') {
+                setSelectedFile(file);
+            } else {
+                setSelectedFile(null);
+            }
+        }
+    }, []);
+
+    const createHookAdmin = useCallback(async () => {
+        setAuthToken();
+        if (selectedFile) {
+            try {
+                const res = await adminFetch(url, {
+                    method: 'post',
+                    data: {
+                        title,
+                        file: selectedFile,
+                    },
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                });
+                if (res.status === 200) {
+                    toast.success('Файл успешно создан', {
+                        position: 'bottom-right',
+                        autoClose: 3400,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: 'light',
+                    });
+                    await route.replace('/admin/admin');
+                }
+            } catch (e) {
+                console.error(e);
+            }
+        } else {
+            toast.error('Ошибка! Загрузите картинку', {
+                position: 'bottom-right',
+                autoClose: 3400,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'light',
+            });
+        }
+    }, [selectedFile, title, route]);
+
+    return {
+        setTitle,
+        handleFileChange,
+        cancelCreate,
+        createHookAdmin,
+    };
+}
