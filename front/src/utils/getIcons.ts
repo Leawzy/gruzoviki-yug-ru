32ff3fbe838f996d6d6d7b56c5bdf12aeb30064f
Renth0p <onedostupov@gmail.com>
2023-05-11 17:16:37 +0300

    feat(front): Creating file for mui/icons to exports in another file and optimize file

---
+++ front/src/utils/getIcons.ts
@@ -0,0 +1,21 @@
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import CategoryIcon from '@mui/icons-material/Category';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import RepeatOneIcon from '@mui/icons-material/RepeatOne';
+
+export {
+    AddPhotoAlternateIcon,
+    AddShoppingCartIcon,
+    CategoryIcon,
+    DashboardIcon,
+    ExitToAppIcon,
+    Inventory2Icon,
+    NewspaperIcon,
+    PersonOutlineIcon,
+    RepeatOneIcon,
+};
