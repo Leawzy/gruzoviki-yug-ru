8634dc14d13aee4bdfafb07dcc36a9e3a0974984
Iliya <facyoucraft@gmail.com>
2023-05-11 13:51:45 +0300

    feat(back): add func for record repair

--- back/app/Http/Controllers/Api/v1/OtherController.php
+++ back/app/Http/Controllers/Api/v1/OtherController.php
@@ -8,7 +8,9 @@ use App\Http\Resources\Other\PostResource;
 use App\Http\Resources\Other\SliderResource;
 use App\Models\Order;
 use App\Models\Post;
+use App\Models\Repair;
 use App\Models\Slider;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -40,5 +42,36 @@ class OtherController extends Controller
         }
 
         return 'null';
+    }
+
+    public function getRecordRepair(Request $request)
+    {
+        $token = JWTAuth::parseToken();
+        $user = $token->authenticate();
+
+        $data = $request->validate([
+            'type' => $request['type'],
+            'brand' => $request['brand'],
+            'model' => $request['model'],
+            'description' => $request['description'],
+            'status' => $request['status'],
+            'date' => $request['date'],
+        ]);
+
+        $repair = Repair::create([
+            'user_id' => $user->id,
+            'type' => $data['type'],
+            'brand' => $data['brand'],
+            'model' => $data['model'],
+            'description' => $data['description'],
+            'status' => $data['status'],
+            'date' => $data['date'],
+        ]);
+
+        $repair->save();
+
+        return response()->json([
+            'message' => "Вы успешно записались"
+        ], 200);
     }
 }
