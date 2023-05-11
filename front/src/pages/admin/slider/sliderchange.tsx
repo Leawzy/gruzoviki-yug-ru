9a4113c84cba2067f4b44e9058c8c3e1e34f3ab9
Renth0p <onedostupov@gmail.com>
2023-05-11 17:23:43 +0300

    feat(front): Rename page to lowercase

---
+++ front/src/pages/admin/slider/sliderchange.tsx
@@ -0,0 +1,123 @@
+import { Button, Dialog, DialogActions, DialogContent, TextField } from '@mui/material';
+import { DataGrid, GridColDef, GridRowsProp, ruRU } from '@mui/x-data-grid';
+import React, { useEffect } from 'react';
+
+import AdminLayout from '../../../components/shared/layouts/AdminLayout';
+import { useHandleFileChangeHook } from '../../../hooks/admin/handlers/useHandleFileChangeHook';
+import { useModalHandlerHook } from '../../../hooks/admin/handlers/useModalHandlerHook';
+import { useSendChangeHook } from '../../../hooks/admin/handlers/useSendChangeHook';
+import { useGetSliderHook } from '../../../hooks/admin/useGetSliderHook';
+import { withAuth } from '../../../utils/withAuth';
+import { withAuthAdmin } from '../../../utils/withAuthAdmin';
+
+function SliderChange() {
+    const { slider } = useGetSliderHook();
+    const { selectedImage, selectedImageUrl, handleFileChange } = useHandleFileChangeHook();
+    const {
+        rows,
+        setRows,
+        selectedRow,
+        setSelectedRow,
+        openModal,
+        handleOpenModal,
+        handleDeleteRow,
+        handleCloseModal,
+    } = useModalHandlerHook([]);
+
+    const useHandleSaveChanges = async () => {
+        try {
+            await useSendChangeHook(
+                '/slider/change',
+                {
+                    id: selectedRow?.id as string,
+                    title: selectedRow?.title as string,
+                    file: selectedImage,
+                },
+                'post'
+            );
+            handleCloseModal();
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    const columns: GridColDef[] = [
+        { field: 'id' },
+        { field: 'title', headerName: 'Название', width: 550 },
+        {
+            field: 'actions',
+            headerName: 'Действие',
+            headerAlign: 'center',
+            align: 'center',
+            width: 300,
+            renderCell: params => {
+                const handleEdit = () => {
+                    handleOpenModal(params.id);
+                };
+
+                const handleDelete = () => {
+                    handleDeleteRow(params.id, 'slider/delete');
+                };
+
+                return (
+                    <>
+                        <Button onClick={handleEdit}>Изменить</Button>
+                        <Button onClick={handleDelete} color="error">
+                            Удалить
+                        </Button>
+                    </>
+                );
+            },
+        },
+    ];
+
+    useEffect(() => {
+        const formattedRows: GridRowsProp = slider.map(item => ({
+            id: item.id,
+            title: item.name,
+            img: item.img,
+        }));
+        setRows(formattedRows);
+    }, [slider]);
+
+    return (
+        <AdminLayout>
+            <div style={{ height: 900, width: '100%' }}>
+                <DataGrid
+                    localeText={ruRU.components.MuiDataGrid.defaultProps.localeText}
+                    rows={rows}
+                    columns={columns}
+                />
+                <Dialog open={openModal} onClose={handleCloseModal}>
+                    <DialogContent>
+                        <TextField
+                            label="Название Cлайдера"
+                            value={(selectedRow?.title as string) || ''}
+                            onChange={e => {
+                                setSelectedRow(prev => ({ ...prev, title: e.target.value }));
+                            }}
+                        />
+                        {selectedImage === undefined ? (
+                            <img
+                                src={selectedRow?.img as string}
+                                height={280}
+                                alt={selectedRow?.title as string}
+                            />
+                        ) : (
+                            <img src={selectedImageUrl} height={280} alt="Выбранное изображение" />
+                        )}
+                        <input type="file" accept="image/webp" onChange={handleFileChange} />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleCloseModal}>Отмена</Button>
+                        <Button onClick={useHandleSaveChanges} variant="contained" color="primary">
+                            Сохранить
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        </AdminLayout>
+    );
+}
+
+export default withAuth(withAuthAdmin(SliderChange));
