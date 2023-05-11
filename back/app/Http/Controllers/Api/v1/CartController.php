7018a8cf05dcc2f9854f102e6f3fcb0684d00735
Iliya <facyoucraft@gmail.com>
2023-05-11 11:33:47 +0300

    feat(back): Create order add func

---
+++ back/app/Http/Controllers/Api/v1/CartController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function createOrder(Request $request)
+    {
+        $data = $request->validate([
+           'userId' => ['request', 'integer'],
+            'date' => ['request'],
+            'total' => ['request', 'integer'],
+            'delivery' => ['request', 'string'],
+            'paymentMethod' => ['request', 'string'],
+            'status' => ['request', 'string'],
+        ]);
+
+        $order = Order::create([
+           'user_id' => $data['userId'],
+           'date' => $data['date'],
+           'total' => $data['total'],
+           'delivery' => $data['delivery'],
+           'paymentMethod' => $data['paymentMethod'],
+           'status' => $data['status'],
+        ]);
+
+        $order->save();
+
+        return response()->json([
+            'message' => "Заказ успешно создан"
+        ], 200);
+    }
+}
--- back/database/migrations/2023_04_27_200526_create_orders_table.php
+++ back/database/migrations/2023_04_27_200526_create_orders_table.php
@@ -13,13 +13,13 @@ return new class extends Migration {
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('userId')
+            $table->unsignedBigInteger('user_id')
                 ->nullable();
             $table->foreign('userId')
                 ->references('id')
                 ->on('users');
 
-            $table->dateTime('data')
+            $table->dateTime('date')
                 ->nullable();
             $table->string('total');
             $table->string('delivery');
