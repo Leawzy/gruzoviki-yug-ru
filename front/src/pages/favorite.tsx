4610c7bb4785b59cb4642955eda08167a7e57397
Renth0p <onedostupov@gmail.com>
2023-05-12 18:12:31 +0300

    feat(front): Creating functional component favorite.tsx

--- front/src/pages/favorite.tsx
+++ front/src/pages/favorite.tsx
@@ -1,5 +1,41 @@
 import React from 'react';
 
-export default function Favorite() {
-    return <div>1</div>;
+import ProductCard from '../components/core/card/ProductCard';
+import BaseLayout from '../components/shared/layouts/BaseLayout';
+import { useGetFavoriteHook } from '../hooks/favorites/useGetFavoriteHook';
+import { Product } from '../types/ProductType';
+import { withAuth } from '../utils/withAuth';
+
+function FavoritePage() {
+    const { favoriteList, loading } = useGetFavoriteHook();
+
+    if (!loading) {
+        return <div>Загрузка...</div>;
+    }
+
+    return (
+        <BaseLayout>
+            <div>
+                <h1>Избранный товар</h1>
+                <div>
+                    {/* eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access */}
+                    {favoriteList?.products?.map((item: Product) => (
+                        <ProductCard
+                            key={item.id}
+                            id={item.id}
+                            title={item.title}
+                            shortDesc={item.shortDesc}
+                            img={item.img}
+                            quantity={item.quantity}
+                            brand={item.brand}
+                            sale={item.sale}
+                            price={item.price}
+                        />
+                    ))}
+                </div>
+            </div>
+        </BaseLayout>
+    );
 }
+
+export default withAuth(FavoritePage);
