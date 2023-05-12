3ea2dd793a9a08bc2316e8c179eb4c5792713c51
Renth0p <onedostupov@gmail.com>
2023-05-12 18:12:51 +0300

    feat(front): Add new icons

--- front/src/utils/getIcons.ts
+++ front/src/utils/getIcons.ts
@@ -3,10 +3,14 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import CategoryIcon from '@mui/icons-material/Category';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import Inventory2Icon from '@mui/icons-material/Inventory2';
 import NewspaperIcon from '@mui/icons-material/Newspaper';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import RepeatOneIcon from '@mui/icons-material/RepeatOne';
+import ShareIcon from '@mui/icons-material/Share';
 
 export {
     AddPhotoAlternateIcon,
@@ -14,8 +18,12 @@ export {
     CategoryIcon,
     DashboardIcon,
     ExitToAppIcon,
+    FavoriteBorderIcon,
+    FavoriteIcon,
+    HelpOutlineIcon,
     Inventory2Icon,
     NewspaperIcon,
     PersonOutlineIcon,
     RepeatOneIcon,
+    ShareIcon,
 };
