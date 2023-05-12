59ee301b46fe1cd2aebf1f438c9682aa5f15224a
Renth0p <onedostupov@gmail.com>
2023-05-12 18:17:08 +0300

    feat(front): Add art to props

--- front/src/components/features/cart/index.tsx
+++ front/src/components/features/cart/index.tsx
@@ -38,6 +38,7 @@ export default function Cart() {
                                 img={cartItem.img}
                                 price={cartItem.price}
                                 quantity={cartItem.quantity}
+                                art={cartItem.art}
                             />
                         ))}
                     </div>
