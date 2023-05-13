4dac6c007cf4184c95945fa3109764645a86daab
Renth0p <onedostupov@gmail.com>
2023-05-13 20:16:10 +0300

    feat(front): Check is img for news null or not

--- front/src/pages/news/[newsId].tsx
+++ front/src/pages/news/[newsId].tsx
@@ -1,9 +1,12 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React, { CSSProperties, useEffect, useState } from 'react';
 import { RotateLoader } from 'react-spinners';
 
 import { apiFetch } from '../../axios/global';
+import BaseLayout from '../../components/shared/layouts/BaseLayout';
 import { News } from '../../types/NewsType';
+import cn from './style.module.scss';
 
 const override: CSSProperties = {
     margin: '22% 48%',
@@ -35,7 +38,21 @@ function ProductPage() {
             <RotateLoader cssOverride={override} color="#4c96e3" size={15} speedMultiplier={1} />
         );
 
-    return <div>{news.title}</div>;
+    return (
+        <BaseLayout>
+            <div className={cn.container}>
+                <h1 className={cn.title}>{news.title}</h1>
+                <div className={cn.imageContainer}>
+                    {news.img === null && undefined ? (
+                        ''
+                    ) : (
+                        <Image src={news.img} alt={news.title} width={600} height={400} />
+                    )}
+                </div>
+                <p className={cn.description}>{news.description}</p>
+            </div>
+        </BaseLayout>
+    );
 }
 
 export default ProductPage;
