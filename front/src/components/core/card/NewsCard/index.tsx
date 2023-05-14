5503e3dbd65411bed484a0f0c5bdccfa3ea3b948
Renth0p <onedostupov@gmail.com>
2023-05-14 17:38:17 +0300

    fix(front): Fixed image size

--- front/src/components/core/card/NewsCard/index.tsx
+++ front/src/components/core/card/NewsCard/index.tsx
@@ -6,12 +6,11 @@ import cn from './style.module.scss';
 
 export default function NewsCard({ title, img, createdAt, id, shortDesc }: NewsCardIF) {
     const formattedDate = new Date(createdAt).toLocaleString('ru-RU', { dateStyle: 'full' });
-
     return (
         <Link href={`/news/${id}`}>
             <div className={cn.cardNews}>
                 <div className={cn.cardHeader}>
-                    <img src={img} alt="News card" className={cn.cardImage} width="600" />
+                    <img src={img} alt="News card" className={cn.cardImage} />
                 </div>
                 <div className={cn.cardBody}>
                     <h4>{title}</h4>
