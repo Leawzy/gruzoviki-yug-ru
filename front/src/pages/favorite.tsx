5c2e4e293da99d006fd00186a70911f2e10f7408
Renth0p <onedostupov@gmail.com>
2023-05-16 22:28:50 +0300

    feat(front): Add preloader

--- front/src/pages/favorite.tsx
+++ front/src/pages/favorite.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
+import { RotateLoader } from 'react-spinners';
 
 import ProductCard from '../components/core/card/ProductCard';
 import BaseLayout from '../components/shared/layouts/BaseLayout';
@@ -6,18 +7,28 @@ import { useGetFavoriteHook } from '../hooks/favorites/useGetFavoriteHook';
 import { Product } from '../types/ProductType';
 import { withAuth } from '../utils/withAuth';
 
+const override: CSSProperties = {
+    margin: '22% 48%',
+};
+
 function FavoritePage() {
     const { favoriteList, loading } = useGetFavoriteHook();
 
     if (!loading) {
-        return <div>Загрузка...</div>;
+        return (
+            <RotateLoader cssOverride={override} color="#4c96e3" size={15} speedMultiplier={1} />
+        );
     }
 
     return (
         <BaseLayout>
             <div>
-                <h1>Избранный товар</h1>
-                <div>
+                <div className="favoriteTitle">
+                    <h1>Избранный товар</h1>
+                    {/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */}
+                    <p>Всего в избранном: {favoriteList?.products?.length}</p>
+                </div>
+                <div className="favoriteWrapper">
                     {/* eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access */}
                     {favoriteList?.products?.map((item: Product) => (
                         <ProductCard
@@ -30,6 +41,8 @@ function FavoritePage() {
                             brand={item.brand}
                             sale={item.sale}
                             price={item.price}
+                            art={item.art}
+                            description={item.description}
                         />
                     ))}
                 </div>
