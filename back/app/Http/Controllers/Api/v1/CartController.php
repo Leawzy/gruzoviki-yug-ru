da1dee4d79cc8b9f8c0166fb067e531e0ec7af2e
Iliya <facyoucraft@gmail.com>
2023-05-17 17:01:36 +0300

    feat(back): added change number of items when ordering, and route for create order

--- back/app/Http/Controllers/Api/v1/CartController.php
+++ back/app/Http/Controllers/Api/v1/CartController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\OrderProduct;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
@@ -11,7 +13,7 @@ class CartController extends Controller
     public function createOrder(Request $request)
     {
         $data = $request->validate([
-           'userId' => ['request', 'integer'],
+            'userId' => ['request', 'integer'],
             'date' => ['request'],
             'total' => ['request', 'integer'],
             'delivery' => ['request', 'string'],
@@ -20,15 +22,28 @@ class CartController extends Controller
         ]);
 
         $order = Order::create([
-           'user_id' => $data['userId'],
-           'date' => $data['date'],
-           'total' => $data['total'],
-           'delivery' => $data['delivery'],
-           'payment_method' => $data['paymentMethod'],
-           'status' => $data['status'],
+            'user_id' => $data['userId'],
+            'date' => $data['date'],
+            'total' => $data['total'],
+            'delivery' => $data['delivery'],
+            'payment_method' => $data['paymentMethod'],
+            'status' => $data['status'],
         ]);
 
         $order->save();
+
+        $products = $request['products'];
+
+        foreach ($products as $product) {
+            $orderProduct = new OrderProduct();
+            $orderProduct->order_id = $order->order_id;
+            $orderProduct->product_id = $product['id'];
+            $orderProduct->save();
+
+            $productModel = Product::find($product['id']);
+            $productModel->quantity = -$product['quantity'];
+            $productModel->save();
+        }
 
         return response()->json([
             'message' => "Заказ успешно создан"
