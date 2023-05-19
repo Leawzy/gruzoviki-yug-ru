02a4b1a4a99d4522faedabb2f0dbc2ea582d88f5
Renth0p <onedostupov@gmail.com>
2023-05-19 12:59:20 +0300

    feat(front): Add new rule for eternal operator

--- front/src/components/shared/Profile/ProfileOrders/index.tsx
+++ front/src/components/shared/Profile/ProfileOrders/index.tsx
@@ -6,56 +6,69 @@ import cn from './style.module.scss';
 export default function ProfileOrders({ orders }: { orders: OrderTypeIF[] }) {
     return (
         <>
-            {orders?.map((order: OrderTypeIF) => (
-                <article key={order.id} className={cn.orderCard}>
-                    <div className={cn.orderCardTop}>
-                        <div className={cn.orderCardTopInfo}>
-                            <p>
-                                Статус заказа от{' '}
-                                {order.date === null ? 'Дата не указана' : order.date}
-                            </p>
-                            <p>№{order.id}</p>
-                        </div>
-                        <p>{order.total} руб.</p>
-                    </div>
-                    <div className={cn.orderCardBottom}>
-                        <div>
-                            <div
-                                className={`${cn.orderCardStatus} ${
-                                    order.status === 'В обработке'
-                                        ? `${cn.orderCardStatusWorking}`
-                                        : ''
-                                }`}
-                            >
-                                <p>{order.status}</p>
+            {orders?.map((order: OrderTypeIF) => {
+                const formattedDate = new Date(order.date).toLocaleString('ru-RU', {
+                    dateStyle: 'full',
+                });
+                return (
+                    <article key={order.id} className={cn.orderCard}>
+                        <div className={cn.orderCardTop}>
+                            <div className={cn.orderCardTopInfo}>
+                                <p>
+                                    Статус заказа от{' '}
+                                    <span>
+                                        {formattedDate === null ? 'Дата не указана' : formattedDate}
+                                    </span>
+                                </p>
+                                <p>№{order.id}</p>
                             </div>
-                            <p className={cn.orderCardDelivery}>{order.delivery}</p>
+                            <p>{order.total} руб.</p>
+                        </div>
+                        <div className={cn.orderCardBottom}>
                             <div>
-                                <ul className={cn.orderCardList}>
-                                    <li className={cn.orderCardBottomInfo}>
-                                        Доставка:{' '}
-                                        {order.date === null ? 'Дата не указана' : order.date}
-                                    </li>
-                                    <li className={cn.orderCardBottomInfo}>
-                                        Тип оплаты: {order.paymentMethod}
-                                    </li>
-                                </ul>
+                                <div
+                                    className={`${cn.orderCardStatus} ${
+                                        order.status === 'В обработке'
+                                            ? `${cn.orderCardStatusWorking}`
+                                            : ''
+                                    } ${
+                                        order.status === 'Ожидает'
+                                            ? `${cn.orderCardStatusWait}`
+                                            : ''
+                                    }`}
+                                >
+                                    <p>{order.status}</p>
+                                </div>
+                                <p className={cn.orderCardDelivery}>{order.delivery}</p>
+                                <div>
+                                    <ul className={cn.orderCardList}>
+                                        <li className={cn.orderCardBottomInfo}>
+                                            Доставка:{' '}
+                                            {formattedDate === null
+                                                ? 'Дата не указана'
+                                                : formattedDate}
+                                        </li>
+                                        <li className={cn.orderCardBottomInfo}>
+                                            Тип оплаты: {order.paymentMethod}
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
-                        </div>
-                        <div>
                             <div>
-                                <ul>
-                                    <li>Название товара</li>
-                                    {/* eslint-disable-next-line @typescript-eslint/no-unsafe-call */}
-                                    {order.products.map((item: OrderTypeIF) => (
-                                        <li key={item.id}>{item.title}</li>
-                                    ))}
-                                </ul>
+                                <div>
+                                    <ul>
+                                        <li>Название товара</li>
+                                        {/* eslint-disable-next-line @typescript-eslint/no-unsafe-call */}
+                                        {order.products.map((item: OrderTypeIF) => (
+                                            <li key={item.id}>{item.title}</li>
+                                        ))}
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </article>
-            ))}
+                    </article>
+                );
+            })}
         </>
     );
 }
