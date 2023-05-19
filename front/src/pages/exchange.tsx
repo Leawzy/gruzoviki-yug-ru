0ca35e7bc16329d300840a1034de34561c2d5060
Renth0p <onedostupov@gmail.com>
2023-05-19 12:55:58 +0300

    feat(front): Add new page

---
+++ front/src/pages/exchange.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Exchange from '../components/features/exchange';
+import BaseLayout from '../components/shared/layouts/BaseLayout';
+
+export default function ExChangePage() {
+    return (
+        <BaseLayout>
+            <Exchange />
+        </BaseLayout>
+    );
+}
