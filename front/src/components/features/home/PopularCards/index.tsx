a974da11ce878f6f666476851c9ba4df75129d61
Renth0p <onedostupov@gmail.com>
2023-05-19 18:20:09 +0300

    feat(front): Add new skeleton loading

--- front/src/components/features/home/PopularCards/index.tsx
+++ front/src/components/features/home/PopularCards/index.tsx
@@ -1,41 +1,54 @@
+import { Box, Grid, Skeleton } from '@mui/material';
 import React from 'react';
 
 import { usePopularList } from '../../../../hooks/cards/usePopularCardHook';
 import ProductCard from '../../../core/card/ProductCard';
-import SkeletonCard from './SkeletonContainer';
 import cn from './style.module.scss';
 
 export default function PopularCards() {
     const { productList, loading } = usePopularList();
 
-    if (!loading) {
-        productList.map(item => (
-            <div key={item.id}>
-                <SkeletonCard />
-            </div>
-        ));
-    }
-
     return (
         <main className={cn.mainSection}>
             <h1 className={cn.mainSectionTitle}>Популярные товары</h1>
-            <div className={cn.mainPopularCardsCatalog}>
-                {productList.map(product => (
-                    <ProductCard
-                        key={product.id}
-                        img={product.img}
-                        id={product.id}
-                        slug={product.slug}
-                        shortDesc={product.shortDesc}
-                        quantity={product.quantity}
-                        brand={product.brand}
-                        title={product.title}
-                        sale={product.sale}
-                        price={product.price}
-                        art={product.art}
-                    />
-                ))}
-            </div>
+            {loading === true ? (
+                <div className={cn.mainPopularCardsCatalog}>
+                    {productList.map(product => (
+                        <ProductCard
+                            key={product.id}
+                            img={product.img}
+                            id={product.id}
+                            slug={product.slug}
+                            shortDesc={product.shortDesc}
+                            quantity={product.quantity}
+                            brand={product.brand}
+                            title={product.title}
+                            sale={product.sale}
+                            price={product.price}
+                            art={product.art}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <Grid container wrap="nowrap">
+                    {Array.from(new Array(4)).map((item, index) => (
+                        <Box key={index} sx={{ width: 360, marginRight: 0.5 }}>
+                            <Box sx={{ p: 2 }}>
+                                <Skeleton variant="rectangular" width={242} height={180} />
+                                <Skeleton sx={{ mt: 0.5 }} width="60%" />
+                                <Skeleton sx={{ mt: 2 }} width="70%" />
+                                <Skeleton width="60%" />
+                                <Skeleton
+                                    sx={{ mt: 2 }}
+                                    variant="rectangular"
+                                    width={242}
+                                    height={60}
+                                />
+                            </Box>
+                        </Box>
+                    ))}
+                </Grid>
+            )}
         </main>
     );
 }
