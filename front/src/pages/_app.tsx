3f4d28c580ed74f4cef5e97061cdd92770fae6a5
Renth0p <onedostupov@gmail.com>
2023-05-20 15:17:23 +0300

    feat(front): Add global toastContainer for all app

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -4,21 +4,30 @@ import '../styles/index.scss';
 import { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
 import SEO from 'next-seo.config';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 
 import store from '../redux/store';
 import { fetchFavorites } from '../redux/actions';
 import { parseCookies } from 'nookies';
+import { ToastContainer } from 'react-toastify';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const cookies = parseCookies();
-    const { token } = cookies;
-    // @ts-ignore
-    token ? store.dispatch(fetchFavorites()) : '';
     return (
         <Provider store={store}>
             <DefaultSeo {...SEO} />
+            <ToastContainer
+                position="bottom-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                closeOnClick
+                pauseOnHover
+                draggable
+                newestOnTop={false}
+                rtl={false}
+                limit={1}
+                closeButton={undefined}
+            />
             <Component {...pageProps} />
         </Provider>
     );
