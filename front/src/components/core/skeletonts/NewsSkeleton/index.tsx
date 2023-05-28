8965235660a8d6918c8583ad2e8686a2b7e43356
Renth0p <onedostupov@gmail.com>
2023-05-28 19:53:06 +0300

    feat(front): Add classNames for skeleton

--- front/src/components/core/skeletonts/NewsSkeleton/index.tsx
+++ front/src/components/core/skeletonts/NewsSkeleton/index.tsx
@@ -1,13 +1,24 @@
 import { Box, Grid, Skeleton } from '@mui/material';
 import React from 'react';
 
+import cn from '../style.module.scss';
+
 function NewsSkeleton() {
     return (
-        <Grid container wrap="nowrap">
+        <Grid container wrap="nowrap" className={cn.skeletonWrap}>
             {Array.from(new Array(3)).map((item, index) => (
-                <Box key={index} sx={{ width: 560, marginRight: 0.5 }}>
+                <Box
+                    key={index}
+                    className={cn.skeletonContent}
+                    sx={{ width: 560, marginRight: 0.5 }}
+                >
                     <Box sx={{ p: 2 }}>
-                        <Skeleton variant="rectangular" width={342} height={180} />
+                        <Skeleton
+                            className={cn.skeletonContentImg}
+                            variant="rectangular"
+                            width={342}
+                            height={180}
+                        />
                         <Skeleton sx={{ mt: 0.5 }} width="78%" />
                         <Skeleton sx={{ mt: 2 }} width="78%" />
                         <Skeleton width="78%" />
