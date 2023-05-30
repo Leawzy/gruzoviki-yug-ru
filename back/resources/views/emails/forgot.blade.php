a73a9dbb4107d2a322282467d877222d6a098ef8
Iliya <facyoucraft@gmail.com>
2023-05-30 10:30:42 +0300

    feat(front): add style to mails

--- back/resources/views/emails/forgot.blade.php
+++ back/resources/views/emails/forgot.blade.php
@@ -1,12 +1,66 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="ru">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Восстановление пароля</title>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+    <title>Appointment Reminder Email Template</title>
+    <meta name="description" content="Appointment Reminder Email Template">
 </head>
-<body>
-<h1>Восстановление пароля</h1>
-<p><strong>Ваш новый пароль:</strong></p>
-<p>{{ $newPassword }}</p>
+<style>
+    a:hover {
+        text-decoration: underline !important;
+    }
+</style>
+
+<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+<table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+       style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+    <tr>
+        <td>
+            <table style="background-color: #f2f3f8; max-width:670px; margin:0 auto;" width="100%" border="0"
+                   align="center" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td style="height:80px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td style="height:20px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                               style="max-width:670px; background:#fff; border-radius:3px;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);padding:0 40px;">
+                            <tr>
+                                <td style="height:40px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0 15px; text-align:center;">
+                                    <h1 style="color:#1e1e2d; font-weight:400; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">
+                                        Восстановление пароля</h1>
+                                    <span style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece;
+                                        width:100px;"></span>
+                                    <p>Ваш новый пароль: {{ $newPassword }}</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="height:40px;">&nbsp;</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="height:20px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center;">
+                        <p style="font-size:14px; color:#455056bd; line-height:18px; margin:0 0 0; padding-bottom: 15px;">
+                            &copy; <strong>грузовики-юг.рф</strong>
+                        </p>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
 </body>
+
 </html>
--- back/resources/views/emails/order.blade.php
+++ back/resources/views/emails/order.blade.php
@@ -1,40 +1,89 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="ru">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Заказ</title>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+    <title>Appointment Reminder Email Template</title>
+    <meta name="description" content="Appointment Reminder Email Template">
 </head>
-<body>
-<h1>Заказ № {{ $id }}</h1>
+<style>
+    a:hover {
+        text-decoration: underline !important;
+    }
+</style>
 
-<p><strong>Ваш заказ:</strong></p>
-
-<table>
-    <thead>
+<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+<table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+       style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
     <tr>
-        <th>Наименование</th>
-        <th>Цена, ₽</th>
-        <th>Кол-во</th>
-        <th>Сумма, ₽</th>
+        <td>
+            <table style="background-color: #f2f3f8; max-width:670px; margin:0 auto;" width="100%" border="0"
+                   align="center" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td style="height:80px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td style="height:20px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                               style="max-width:670px; background:#fff; border-radius:3px;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);padding:0 40px;">
+                            <tr>
+                                <td style="height:40px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0 15px; text-align:center;">
+                                    <h1 style="color:#1e1e2d; font-weight:400; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">
+                                        Заказ №{{ $id }}</h1>
+                                    <span style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece;
+                                        width:100px;"></span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <table style="width: 100%; border: 1px solid #ededed">
+                                        <thead>
+                                        <tr>
+                                            <th>Наименование</th>
+                                            <th>Цена, ₽</th>
+                                            <th>Кол-во</th>
+                                            <th>Сумма, ₽</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($products as $product)
+                                            <tr>
+                                                <td>{{ $product['title'] }}</td>
+                                                <td>{{ $product['price'] }}</td>
+                                                <td>{{ $product['quantity'] }}</td>
+                                                <td>{{ $product['price'] * $product['quantity'] }}</td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="height:40px;">&nbsp;</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="height:20px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center;">
+                        <p style="font-size:14px; color:#455056bd; line-height:18px; margin:0 0 0; padding-bottom: 15px;">
+                            &copy; <strong>грузовики-юг.рф</strong>
+                        </p>
+                    </td>
+                </tr>
+            </table>
+        </td>
     </tr>
-    </thead>
-    <tbody>
-    @foreach($products as $product)
-        <tr>
-            <td>{{ $product['title'] }}</td>
-            <td>{{ $product['price'] }}</td>
-            <td>{{ $product['quantity'] }}</td>
-            <td>{{ $product['price'] * $product['quantity'] }}</td>
-        </tr>
-    @endforeach
-    </tbody>
-    <tfoot>
-    <tr>
-        <td colspan="3">Итого к оплате:</td>
-        <td>{{ $total }}</td>
-    </tr>
-    </tfoot>
 </table>
-
 </body>
+
 </html>
